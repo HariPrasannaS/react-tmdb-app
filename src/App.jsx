6434342d8761b2737f54bd84261323742766806a
HariPrasannaS <hariprasannas.23eie@kongu.edu>
2025-06-25 13:43:32 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useEffect,useState } from "react";
+import Search from "./components/Search";
+
+const API_BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+function App() {
+  const[searchTerm,setSearchTerm]=useState('');
+  const[errorMessage,setErrorMessage]=useState('');
+
+const fetchMovies= async () =>{
+  try{
+    // throw new Error("Failed to fetch Movies")
+    const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+    const response = await fetch(endpoint,API_OPTIONS);
+
+    if(!response){
+       throw new Error("Failed to fetch Movies");
+    }
+    const data = await response.json();
+
+    if(data.Response === "False"){
+      setErrorMessage(data.Error || "Failed to fetch Movies")
+    }
+    // console.log(data);
+  }catch(error){
+      console.log(`Error fetching movie: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again later.")
+  }
+};
+
+
+useEffect(()=> {
+  fetchMovies();
+}, []);
+
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="hero.png" alt="" />
+          <h1>Find<span className="text-gradient">Movies</span> You'll Enjoy Without Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        </header>
+        <section className="all-movies">
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App
